ece641cc5f231f50724274afc736e8c00b0cfb9d
elenapronyushkina <e.pronyushkina@gmail.com>
2021-11-27 11:32:46 -0600

    second commit: env, login tests

--- pages/login.page.js
+++ pages/login.page.js
@@ -4,6 +4,9 @@ class LoginPage extends Page {
     get inputUsername() {return $('[id="Email"]');}
     get inputPassword() {return $('[id="Password"]');}
     get buttonSubmit() {return $('.login-button');}
+    get notification() {return $('div.validation-summary-errors>span');}
+    get notificationCustomer() {return $('div.validation-summary-errors>span+ul>li');}
+    get invalidEmail() {return $('.inputs>span>span');}
 
     async login (username, password) {
         await this.inputUsername.setValue(username);
--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -2,10 +2,37 @@ import LoginPage from '../pages/login.page';
 import ProfilePage from '../pages/profile.page'
 
 describe('Auth', function () {
-    it ('Succsessful log in', async function () {
+
+    beforeEach(async function (){
         await LoginPage.open();
-        await LoginPage.login('test@shop.com', '123456');
+    });
+
+    it ('Succsessful log in', async function () {
+        await LoginPage.login(process.env.LOGIN, process.env.PASSWORD);
         await expect(ProfilePage.profile)
             .toBeDisplayed();
     });
+
+    it ('Log in with not registered email', async function () {
+        await LoginPage.login('testInvalid@shop.com', 'Invalid');
+        await expect(LoginPage.notification)
+            .toHaveText('Login was unsuccessful. Please correct the errors and try again.');
+        await expect(LoginPage.notificationCustomer)
+            .toHaveText('No customer account found');
+    });
+
+    it ('Log in with invalid password', async function () {
+        await LoginPage.login(process.env.LOGIN, '12Invalid');
+        await expect(LoginPage.notification)
+            .toHaveText('Login was unsuccessful. Please correct the errors and try again.');
+        await expect(LoginPage.notificationCustomer)
+            .toHaveText('The credentials provided are incorrect');
+    });
+
+    it ('Log in with invalid email', async function () {
+        await LoginPage.login('test', '12Invalid');
+        await expect(LoginPage.invalidEmail)
+            .toHaveText('Please enter a valid email address.');
+    });
+
 });
